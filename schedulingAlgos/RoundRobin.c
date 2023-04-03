bd86da91fd12114eb87f662df820918c475f4ae8
Chamarti Sreemannarayana <schamart2@gitam.in>
2023-04-03 19:04:52 +0530

    Changed Round Robin Code

--- schedulingAlgos/RoundRobin.c
+++ schedulingAlgos/RoundRobin.c
@@ -1,53 +1,63 @@
-// round robin scheduling algorithm
-
 #include <stdio.h>
 
-int main()
-{
-    int cnt, j, n, t, remain, flag = 0, tq;
-    int wt = 0, tat = 0, at[10], bt[10], rt[10];
-    printf("Enter Total Process:\t ");
-    scanf("%d", &n);
-    remain = n;
-    for (cnt = 0; cnt < n; cnt++)
-    {
-        printf("Enter Arrival Time and Burst Time for Process Process Number %d :", cnt + 1);
-        scanf("%d %d", &at[cnt], &bt[cnt]);
-        rt[cnt] = bt[cnt];
+void printTable(int at[], int bt[], int wt[], int tat[], int n) {
+    printf("\nProcess No\tArrival Time\tBurst Time\tTAT\t\tWaiting Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, at[i], bt[i], tat[i], wt[i]);
     }
-    printf("Enter Time Quantum: ");
-    scanf("%d", &tq);
-    printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n");
-    for (t = 0, cnt = 0; remain != 0;)
-    {
-        if (rt[cnt] <= tq && rt[cnt] > 0)
-        {
-            t += rt[cnt];
-            rt[cnt] = 0;
-            flag = 1;
-        }
-        else if (rt[cnt] > 0)
-        {
-            rt[cnt] -= tq;
-            t += tq;
-        }
-        if (rt[cnt] == 0 && flag == 1)
-        {
-            remain--;
-            printf("P[%d]\t|\t%d\t|\t%d\n", cnt + 1, t - at[cnt], t - at[cnt] - bt[cnt]);
-            wt += t - at[cnt] - bt[cnt];
-            tat += t - at[cnt];
-            flag = 0;
-        }
-        if (cnt == n - 1)
-            cnt = 0;
-        else if (at[cnt + 1] <= t)
-            cnt++;
-        else
-            cnt = 0;
-    }
-    printf("\nAverage Waiting Time= %f\n", wt * 1.0 / n);
-    printf("Avg Turnaround Time = %f", tat * 1.0 / n);
+}
 
-    return 0;
+void main() {
+    int i, NOP, sum = 0, count = 0, y, quant, wt = 0, tat = 0, at[10], bt[10], temp[10];
+    float avg_wt, avg_tat;
+    printf("Total number of processes in the system: ");
+    scanf("%d", &NOP);
+    y = NOP;
+
+    for (i = 0; i < NOP; i++) {
+        printf("\nEnter the arrival and burst time of process[%d]:\n", i + 1);
+        printf("Arrival time: ");
+        scanf("%d", &at[i]);
+        printf("Burst time: ");
+        scanf("%d", &bt[i]);
+        temp[i] = bt[i];
+    }
+
+    printf("Enter the time quantum: ");
+    scanf("%d", &quant);
+
+    printf("\nProcess No\tBurst Time\tTAT\t\tWaiting Time\n");
+
+    for (sum = 0, i = 0; y != 0;) {
+        if (temp[i] <= quant && temp[i] > 0) {
+            sum = sum + temp[i];
+            temp[i] = 0;
+            count = 1;
+        } else if (temp[i] > 0) {
+            temp[i] = temp[i] - quant;
+            sum = sum + quant;
+        }
+        if (temp[i] == 0 && count == 1) {
+            y--;
+            printf("%d\t\t%d\t\t%d\t\t%d\n", i + 1, bt[i], sum - at[i], sum - at[i] - bt[i]);
+            wt = wt + sum - at[i] - bt[i];
+            tat = tat + sum - at[i];
+            count = 0;
+        }
+        if (i == NOP - 1) {
+            i = 0;
+        } else if (at[i + 1] <= sum) {
+            i++;
+        } else {
+            i = 0;
+        }
+    }
+
+    avg_wt = (float) wt / NOP;
+    avg_tat = (float) tat / NOP;
+
+    printf("\nAverage waiting time: %f", avg_wt);
+    printf("\nAverage turnaround time: %f", avg_tat);
+
+    printTable(at, bt, wt, tat, NOP);
 }
